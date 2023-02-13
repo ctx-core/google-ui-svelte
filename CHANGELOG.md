c6f3249dbf6b35eebc2fa4ac967f54e05dca86e5
Brian Takita <brian.takita@gmail.com>
2023-02-13 02:56:49 -0500

    ## 1.1.2
    ### Patch Changes
    
    - fix: import \*/index.svelte

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ctx-core/google-ui-svelte
 
+## 1.1.2
+
+### Patch Changes
+
+- fix: import \*/index.svelte
+
 ## 1.1.1
 
 ### Patch Changes
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import 'svelte'
-export * as Body_GTM from './Body_GTM'
-export * as Head_GTM from './Head_GTM'
+export * as Body_GTM from './Body_GTM/index.svelte'
+export * as Head_GTM from './Head_GTM/index.svelte'
